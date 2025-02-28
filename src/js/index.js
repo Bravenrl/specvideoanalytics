05b4fba01a394f1f6edc1d1d52446ea647c8a46b
Roman Lozinsky <bravenrl@gmail.com>
2025-03-01 01:02:45 +0300

    add popup

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,37 @@ menu.addEventListener('click', () => {
   body.classList.toggle('menu--active');
   header.classList.toggle('menu--active');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href');
+    if (!targetId || targetId === '#') {
+      window.scrollTo({
+        top: window.offsetTop || 0,
+        behavior: 'smooth',
+      });
+      return;
+    }
+
+    const targetSection = document.querySelector(targetId);
+    window.scrollTo({
+      top: targetSection.offsetTop - 50,
+      behavior: 'smooth',
+    });
+  });
+});
+
+const openModalButtons = document.querySelectorAll('.callback__btn--popup');
+const modal = document.querySelector('#modal');
+const closeModalButton = document.querySelector('#close-btn');
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    modal.showModal();
+  });
+});
+
+closeModalButton.addEventListener('click', () => {
+  modal.close();
+});
